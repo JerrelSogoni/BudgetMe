1fcf0f625f24b9e3f43460f12057a27151209b4a
JerrelSogoni <jerrel.sogoni@stonybrook.edu>
2020-07-22 00:19:47 -0400

    format using prettier

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.ENV || 5000;
-const mysql      = require('mysql');
-const connection = require('./model/dbconnection')
+const mysql = require("mysql");
+const connection = require("./model/dbconnection");
 
-app.get('/', (req, res)=>{
-    console.log("testing");
+app.get("/", (req, res) => {
+  console.log("testing");
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
