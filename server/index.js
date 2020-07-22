5cace31d5895d66746ac55553fa2def7d782d8f0
Jerrel Sogoni <jerrel.sogoni@gmail.com>
2020-07-22 00:13:38 -0400

    Create Connection to MongoDb Cluster

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
 const app = express();
 const port = process.ENV || 5000;
-const mysql      = require('mysql');
-const connection = require('./model/dbconnection')
-
-app.get('/', (req, res)=>{
-    console.log("testing");
+const connection = require("./model/DbConnection");
+connection.getConnection();
+app.get("/", (req, res) => {
+  console.log("testing");
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
